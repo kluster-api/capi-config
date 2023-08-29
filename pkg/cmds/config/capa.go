2d86828d453679c1e3d316fbfb9e4cc662dfc709
rasel <rasel@appscode.com>
2023-08-29 16:31:45 +0600

    Add machinepool role, controlplane role, scaling, cluster annotation for capa
    Signed-off-by: rasel <rasel@appscode.com>

--- pkg/cmds/config/capa.go
+++ pkg/cmds/config/capa.go
@@ -29,8 +29,91 @@ import (
 	"sigs.k8s.io/yaml"
 )
 
+const (
+	awsManagedControlPlane     = "AWSManagedControlPlane"
+	awsManagedMachinePool      = "AWSManagedMachinePool"
+	machinePool                = "MachinePool"
+	cluster                    = "Cluster"
+	controlplaneRoleAnnotation = "eks.amazonaws.com/controlplane-role"
+)
+
+func managedCPCIDR(ri *parser.ResourceInfo, vpcCidr string) error {
+	netcfg := map[string]any{
+		"vpc": map[string]any{
+			"cidrBlock": vpcCidr,
+		},
+	}
+	if err := unstructured.SetNestedMap(ri.Object.UnstructuredContent(), netcfg, "spec", "network"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func managedCPRole(ri *parser.ResourceInfo, roleName string) error {
+	if err := unstructured.SetNestedField(ri.Object.UnstructuredContent(), roleName, "spec", "roleName"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func managedMPScaling(ri *parser.ResourceInfo, minNodeCount, maxNodeCount int64) error {
+	scaling := map[string]any{
+		"minSize": minNodeCount,
+		"maxSize": maxNodeCount,
+	}
+	if err := unstructured.SetNestedMap(ri.Object.UnstructuredContent(), scaling, "spec", "scaling"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func managedMPRole(ri *parser.ResourceInfo, roleName string) error {
+	if err := unstructured.SetNestedField(ri.Object.UnstructuredContent(), roleName, "spec", "roleName"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func clusterAnnotations(ri *parser.ResourceInfo, managedControlplaneRole string) error {
+	if err := unstructured.SetNestedField(ri.Object.UnstructuredContent(), managedControlplaneRole, "metadata", "annotations", controlplaneRoleAnnotation); err != nil {
+		return err
+	}
+	return nil
+}
+
+type validationHelper struct {
+	isFound                 map[string]bool
+	managedControlplaneRole string
+	managedMachinepoolRole  string
+	vpcCidr                 string
+	minCount, maxCount      int64
+}
+
+func validation(helper validationHelper) error {
+	if !helper.isFound[awsManagedControlPlane] {
+		if helper.vpcCidr != "" {
+			return errors.New("failed to get AWSManagedControlPlane for cidr update")
+		}
+		if helper.managedControlplaneRole != "" {
+			return errors.New("failed to get AWSManagedControlPlane for role configuration")
+		}
+	}
+	if helper.minCount > helper.maxCount {
+		return errors.New("max node count can't be less than min node count")
+	}
+	if helper.managedMachinepoolRole != "" && !helper.isFound[awsManagedMachinePool] {
+		return errors.New("failed to get AWSManagedMachinePool for role configuration")
+	}
+	if !helper.isFound[cluster] && helper.managedControlplaneRole != "" {
+		return errors.New("failed to get ControlPlane to update annotations")
+	}
+	return nil
+}
+
 func NewCmdCAPA() *cobra.Command {
-	var vpcCidr string
+	var vpcCidr, managedControlplaneRole, managedMachinepoolRole string
+	var minNodeCount, maxNodeCount int64
+	isFound := make(map[string]bool)
 	cmd := &cobra.Command{
 		Use:               "capa",
 		Short:             "Configure CAPA network config",
@@ -40,24 +123,52 @@ func NewCmdCAPA() *cobra.Command {
 			if err != nil {
 				return err
 			}
-			if vpcCidr == "" {
-				_, err = os.Stdout.Write(in)
-				return err
-			}
 
 			var out bytes.Buffer
-			var foundCP bool
 			err = parser.ProcessResources(in, func(ri parser.ResourceInfo) error {
-				if ri.Object.GetKind() == "AWSManagedControlPlane" {
-					foundCP = true
-
-					netcfg := map[string]any{
-						"vpc": map[string]any{
-							"cidrBlock": vpcCidr,
-						},
+				if ri.Object.GetKind() == awsManagedControlPlane {
+					isFound[awsManagedControlPlane] = true
+					if vpcCidr != "" {
+						e := managedCPCIDR(&ri, vpcCidr)
+						if e != nil {
+							return e
+						}
 					}
-					if err := unstructured.SetNestedMap(ri.Object.UnstructuredContent(), netcfg, "spec", "network"); err != nil {
-						return err
+					if managedControlplaneRole != "" {
+						e := managedCPRole(&ri, managedControlplaneRole)
+						if e != nil {
+							return e
+						}
+					}
+				}
+
+				if ri.Object.GetKind() == machinePool {
+					isFound[machinePool] = true
+					e := SetMPConfiguration(ri, minNodeCount, maxNodeCount)
+					if e != nil {
+						return e
+					}
+				}
+
+				if ri.Object.GetKind() == awsManagedMachinePool {
+					isFound[awsManagedMachinePool] = true
+					e := managedMPScaling(&ri, minNodeCount, maxNodeCount)
+					if e != nil {
+						return e
+					}
+					if managedMachinepoolRole != "" {
+						e = managedMPRole(&ri, managedMachinepoolRole)
+						if e != nil {
+							return e
+						}
+					}
+				}
+
+				if ri.Object.GetKind() == cluster {
+					isFound[cluster] = true
+					e := clusterAnnotations(&ri, managedControlplaneRole)
+					if e != nil {
+						return e
 					}
 				}
 
@@ -74,8 +185,18 @@ func NewCmdCAPA() *cobra.Command {
 			if err != nil {
 				return err
 			}
-			if !foundCP {
-				return errors.New("control plane not found, check apiVersion")
+
+			// configuration operation validation
+			err = validation(validationHelper{
+				isFound:                 isFound,
+				managedControlplaneRole: managedControlplaneRole,
+				managedMachinepoolRole:  managedMachinepoolRole,
+				vpcCidr:                 vpcCidr,
+				minCount:                minNodeCount,
+				maxCount:                maxNodeCount,
+			})
+			if err != nil {
+				return err
 			}
 
 			_, err = os.Stdout.Write(out.Bytes())
@@ -83,5 +204,9 @@ func NewCmdCAPA() *cobra.Command {
 		},
 	}
 	cmd.Flags().StringVar(&vpcCidr, "vpc-cidr", "", "CIDR block to be used for vpc")
+	cmd.Flags().StringVar(&managedControlplaneRole, "managedcp-role", "", "Managed ControlPlane role for CAPA")
+	cmd.Flags().StringVar(&managedMachinepoolRole, "managedmp-role", "", "Managed MachinePool role for CAPA")
+	cmd.Flags().Int64Var(&minNodeCount, "min-node-count", 1, "Minimum count of nodes in nodepool")
+	cmd.Flags().Int64Var(&maxNodeCount, "max-node-count", 6, "Maximum count of nodes in nodepool")
 	return cmd
 }
